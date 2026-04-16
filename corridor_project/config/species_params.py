0f1988684ccc9449c1949416298a048f8d698112
marion-billy <marion.billy@imbrea.eu>
2026-04-16 07:52:33 +0000

    MAJ

--- corridor_project/config/species_params.py
+++ corridor_project/config/species_params.py
@@ -3,37 +3,95 @@
 # k : Nombre de voisins pour le graphe KNN
 # friction : Coût de passage par classe d'occupation du sol (Common Legend)
 
+# low d0=500 et k=4
+# high d0=5000 et k=12
+
 SPECIES_CONFIG = {
-    'mid_walker': {
-        'name': 'Mammifère terrestre moyen',
+    'low_walker': {
+        'habitat_codes': [10, 20, 30],  
         'graph': {
-            'd0': 2000,
-            'k_neighbors': 8,
+            'd0': 500,
+            'k_neighbors': 4,
         },
         'friction': {
-            10: 1.0,    # Trees : Habitat favorable
-            30: 1.5,    # Grassland : Passage facile
-            40: 5.0,    # Agriculture : Traversée possible mais découverte
-            60: 30.0,   # Bare soil / Impervious : Stressant (bitume/parking)
-            51: 90.0,   # Highways : Très risqué (mortalité routière élevée)
-            80: 100.0,   # Water : Obstacle (sauf si nageur)
-            50: 999.0,  # Built-up : Barrière quasi-infranchissable (Bâtiments)
+            10: 1,    # Trees : Habitat favorable
+            20: 5,    # Shrubland
+            30: 10,    # Grassland : Passage facile
+            40: 15,    # Agriculture : Traversée possible mais découverte
+            60: 20,   # Bare soil / Impervious : Stressant (bitume/parking)
+            54: 25,   # Pedestrian roads
+            90: 30,   # Wetlands
+            50: 50,  # Built-up
+            53: 100,   # Petites routes
+            52: 10000,   # Highways : Très risqué (mortalité routière élevée)
+            80: 10000,   # Water : Obstacle (sauf si nageur)
+            51: 99999,  # Batiments OSM
         }
     },
-    'mid_flyer': {
-        'name': 'Oiseau',
+    'low_flyer': {
+        'habitat_codes': [10, 20, 30],
+        'graph': {
+            'd0': 500,
+            'k_neighbors': 4,
+        },
+        'friction': {
+            10: 1,    # Trees
+            20: 2,    # Shrubland
+            30: 2,    # Grassland
+            40: 5,    # Agriculture
+            90: 5,    # Wetlands
+            60: 15,    # Bare soil
+            80: 20,    # Water
+            54: 20,   # Pedestrian roads
+            50: 30,  # Built-up
+            53: 50,   # Petites routes
+            52: 1000,    # Highways
+            51: 99999,   # Batiments OSM
+       }
+    },
+
+    
+    'mid_walker': {
+        'habitat_codes': [10, 20, 30],
         'graph': {
             'd0': 2000,
             'k_neighbors': 8,
         },
         'friction': {
-            10: 1.0,    # Trees
-            30: 2.0,    # Grassland
-            40: 5.0,    # Agriculture
-            60: 10.0,    # Bare soil
-            80: 10.0,    # Water
-            50: 20.0,   # Built-up : Dérangement/Obstacle vertical
-            51: 30.0     # Highways : Pas un obstacle pour le vol
+            10: 1,    # Trees : Habitat favorable
+            20: 2,    # Shrubland
+            30: 5,    # Grassland : Passage facile
+            40: 10,    # Agriculture : Traversée possible mais découverte
+            60: 15,   # Bare soil / Impervious : Stressant (bitume/parking)
+            54: 25,   # Pedestrian roads
+            90: 25,   # Wetlands
+            50: 50,  # Built-up
+            53: 100,   # Petites routes
+            52: 10000,   # Highways : Très risqué (mortalité routière élevée)
+            80: 10000,   # Water : Obstacle (sauf si nageur)
+            51: 99999,  # Batiments OSM
+        }
+    },
+    
+    'mid_flyer': {
+        'habitat_codes': [10, 20, 30],
+        'graph': {
+            'd0': 2000,
+            'k_neighbors': 8,
+        },
+        'friction': {
+            10: 1,    # Trees
+            20: 2,    # Shrubland
+            30: 2,    # Grassland
+            40: 5,    # Agriculture
+            90: 5,    # Wetlands
+            60: 10,    # Bare soil
+            80: 10,    # Water
+            54: 20,   # Pedestrian roads
+            50: 50,  # Built-up
+            53: 50,   # Petites routes
+            52: 150,    # Highways : Pas un obstacle pour le vol
+            51: 99999,  # Batiments OSM
         }
     }
 }
